683ad74290fbcd87add7f69f21ba459045ef6a7c
Ogushanakin <ogzhnakn96@gmail.com>
2022-07-11 09:21:26 +0300

    with AutoLayout in mind so cells height can be adjusted dynamically according to the tweet content being wrapped

--- Giggle/Views/GiggleTableViewCell.swift
+++ Giggle/Views/GiggleTableViewCell.swift
@@ -36,15 +36,27 @@ class GiggleTableViewCell: UITableViewCell {
     private let usernameLabel: UILabel = {
        
         let label = UILabel()
+        label.text = "@ogushanakin"
+        label.textColor = .secondaryLabel
         label.font = .systemFont(ofSize: 16, weight: .regular)
         label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let giggleTextContentLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "This is my Mockup giggle. it is going to take multiple lines. i believe some more text is enough but lets add some more anyway.. and cheers youtube.!!"
+        label.numberOfLines = 0
         return label
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.addSubview(avatarImageView)
-        
+        contentView.addSubview(displayName)
+        contentView.addSubview(usernameLabel)
+        contentView.addSubview(giggleTextContentLabel)
         configureConstraints()
     }
     
@@ -57,7 +69,27 @@ class GiggleTableViewCell: UITableViewCell {
             avatarImageView.widthAnchor.constraint(equalToConstant: 50)
         ]
         
+        let displayNameLabelConstraints = [
+            displayName.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 20),
+            displayName.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20)
+        ]
+        
+        let usernameLabelConstraints = [
+            usernameLabel.leadingAnchor.constraint(equalTo: displayName.trailingAnchor, constant: 10),
+            usernameLabel.centerYAnchor.constraint(equalTo: displayName.centerYAnchor)
+        ]
+        
+        let giggleTextContentLabelConstraints = [
+            giggleTextContentLabel.leadingAnchor.constraint(equalTo: displayName.leadingAnchor),
+            giggleTextContentLabel.topAnchor.constraint(equalTo: displayName.bottomAnchor, constant: 10),
+            giggleTextContentLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
+            giggleTextContentLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15)
+        ]
+        
         NSLayoutConstraint.activate(avatarImageViewConstraints)
+        NSLayoutConstraint.activate(displayNameLabelConstraints)
+        NSLayoutConstraint.activate(usernameLabelConstraints)
+        NSLayoutConstraint.activate(giggleTextContentLabelConstraints)
     }
     
     required init?(coder: NSCoder) {
